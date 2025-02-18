3d91b7674126cd4890b1bd38759816cd8d1d8bae
Daniel Byers <mail@danielbyers.net>
2025-02-18 17:58:01 +0000

    Update main() to read the contents of a file based on the name provided in ARGV.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        std::process::exit(1);
+    }
+
+    let filename = &args[1];
+    let contents = fs::read_to_string(filename).unwrap_or_else(|e| {
+        eprintln!("Error reading file {}: {}", filename, e);
+        std::process::exit(1);
+    });
 }
