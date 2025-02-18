95f3e27a87b56e314cf8f207396cf6dace6006db
Daniel Byers <mail@danielbyers.net>
2025-02-18 18:17:16 +0000

    Add first ioc detection method to InjectionDetection. Add regex crate.

--- src/injection_detection.rs
+++ src/injection_detection.rs
@@ -1,5 +1,7 @@
+use regex::Regex;
+
 pub trait Scanner {
-    fn scan_for_ioc(&mut self, input_file: String) -> bool;
+    fn scan_for_ioc(&mut self, input_script: String) -> bool;
 }
 
 #[derive(Default)]
@@ -8,8 +10,26 @@ pub struct InjectionDetection {
     pub enabled: bool
 }
 
+impl InjectionDetection {
+    fn scan_for_array_access(&mut self, input_script: String) {
+        let pattern = r"document\.scripts\[document\.scripts\.length - 2\]\.text";
+        let re = match Regex::new(pattern) {
+            Ok(regex) => regex,
+            Err(e) => {
+                eprintln!("Invalid regex pattern: {}", e);
+                return;
+            }
+        };
+
+        if re.is_match(&input_script) {
+            self.severity += 10;
+        }
+    }
+}
+
 impl Scanner for InjectionDetection {
-    fn scan_for_ioc(&mut self, input_file: String) -> bool {
+    fn scan_for_ioc(&mut self, input_script: String) -> bool {
+        self.scan_for_array_access(input_script);
         return true;
     }
 }
